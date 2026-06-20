d01a89832c186f46e37aa15a50a7674698d80da7
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-21 01:47:01 +0200

    feat: packages.default (nix run) and packages.dockerImage

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Python development environment";
+  description = "Fansly stream recorder";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -9,21 +9,47 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+
+      app = pkgs.writeShellApplication {
+        name = "fansly-recorder";
+        runtimeInputs = with pkgs; [
+          python314
+          python314Packages.playwright
+          playwright-driver.browsers
+          streamlink
+          ffmpeg
+        ];
+        text = ''
+          export PLAYWRIGHT_BROWSERS_PATH="${pkgs.playwright-driver.browsers}"
+          exec "${pkgs.python314}/bin/python" "${./main.py}" "$@"
+        '';
+      };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.git
-          pkgs.ffmpeg
-          pkgs.python314
-          pkgs.python314Packages.playwright
-          pkgs.playwright-driver.browsers
+        buildInputs = with pkgs; [
+          git
+          ffmpeg
+          python314
+          python314Packages.playwright
+          playwright-driver.browsers
         ];
 
         shellHook = ''
           export PATH="${pkgs.streamlink}/bin:$PATH"
           export PLAYWRIGHT_BROWSERS_PATH="${pkgs.playwright-driver.browsers}"
         '';
+      };
+
+      packages.${system} = {
+        default = app;
+
+        dockerImage = pkgs.dockerTools.buildLayeredImage {
+          name = "fansly-recorder";
+          tag = "latest";
+          contents = [ app ];
+          config.Entrypoint = [ "fansly-recorder" ];
+        };
       };
     };
 }
